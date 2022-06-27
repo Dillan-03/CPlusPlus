4d1b4ffb9dd7a2406dc7d80b8dd1b0439e4d619d
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-06-27 16:47:20 +0100

    Data Types

---
+++ dataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Hello World";
+    int age = 20;
+    double gpa = 2.3;
+    bool isFemale = false;
+
+    return 0;
+}
