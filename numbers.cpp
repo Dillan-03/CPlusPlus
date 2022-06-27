b37716033b0707ff730c4d6dfeecba5feb0fb2c7
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-06-27 16:47:38 +0100

    Numbers

---
+++ numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int wNumber = 5;
+    double dnumber = 5.5;
+
+    wNumber += 80;
+
+    cout << wNumber;
+    
+
+    //There's pow, round, floor, ceil, fmax, fmin and other which calculates its corresponding purposes
+    return 0;
+}
