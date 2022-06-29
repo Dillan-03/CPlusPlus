c03061eb07562949a5cda7abe1fda1afbe34693b
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-06-29 19:05:46 +0100

    Arrays

---
+++ arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  int luckNumber[] = {4,8,16,25};
+    
+    luckNumber[2] = 20;
+    cout << luckNumber[2]; 
+
+
+
+
+
+    return 0;
+}
