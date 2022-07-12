5e548c54054ad1c1365b7b15230c50a99ec52cf7
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-07-12 15:04:12 +0100

    updated calc

--- calc.cpp
+++ calc.cpp
@@ -5,14 +5,22 @@ using namespace std;
 int main()
 {
    int num1,num2;
+   char operation;
 
    cout << "Enter first number: ";
    cin >> num1;
 
+   cout << "Enter operation: ";
+   cin >> operation;
+
    cout << "Enter second number: ";
    cin >> num2;
 
-    cout << num1 + num2;
+    if (operation == '+'){cout << "Result is " << (num1 + num2) << endl;}
+    else if (operation == '-'){cout << "Result is " << (num1 - num2) << endl;}
+    else if (operation == '/'){cout << "Result is " << (num1 / num2) << endl;}
+    else if (operation == '*'){cout << "Result is " << (num1 * num2) << endl;}
+    else {cout << "Wrong Operation" << endl;}
 
 
     return 0;
